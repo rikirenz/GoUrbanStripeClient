2ebd018f836afbdaeb863d875756119be65ba9c2
riccardo candido <riccardo.candido@gmail.com>
2020-07-03 11:05:03 +0200

    Init repo with spring boot boilerplates
    Signed-off-by: riccardo candido <riccardo.candido@gmail.com>

---
+++ src/main/java/me/riccardo/candido/GoUrbanStripeClient/GoUrbanStripeClientApplication.java
@@ -0,0 +1,13 @@
+package me.riccardo.candido.GoUrbanStripeClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoUrbanStripeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoUrbanStripeClientApplication.class, args);
+	}
+
+}
